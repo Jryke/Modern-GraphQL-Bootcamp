362faeb74622fca1a47456a3bde1241225f4513d
Jesse Rykerson <rykerson@gmail.com>
2020-11-29 16:54:46 -0500

    GraphQLServer set up and basic type definitions (Schema) and resolvers created

--- src/index.js
+++ src/index.js
@@ -1,1 +1,38 @@
-console.log('Hello GraphQL')
+import { GraphQLServer } from 'graphql-yoga'
+
+// Type definitions (Schema)
+const typeDefs = `
+  type Query {
+    hello: String!
+    name: String!
+    location: String!
+    bio: String!
+  }
+`
+
+// Resolvers
+const resolvers = {
+  Query: {
+    hello() {
+      return 'This is my first query!'
+    },
+    name() {
+      return 'Jesse Rykerson'
+    },
+    location() {
+      return 'Cambridge'
+    },
+    bio() {
+      return 'JavaScript developer'
+    },
+  },
+}
+
+const server = new GraphQLServer({
+  typeDefs,
+  resolvers,
+})
+
+server.start(() => {
+  console.log('The server is up!')
+})
