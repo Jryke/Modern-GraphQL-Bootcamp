c38b71ccedaceea38f43749118cfee76ce77e61d
Jesse Rykerson <rykerson@gmail.com>
2021-01-23 21:13:14 -0500

    Working with Arrays: Part I

--- src/index.js
+++ src/index.js
@@ -6,9 +6,10 @@ import { GraphQLServer } from 'graphql-yoga'
 const typeDefs = `
   type Query {
     greeting(name: String, position: String): String!
+    add(numbers: [Float!]!): Float!
+    grades: [Int!]!
     me: User!
     post: Post!
-    add(a: Float!, b: Float!): Float!
   }
 
   type User {
@@ -38,7 +39,16 @@ const resolvers = {
       }
     },
     add(parent, args, ctx, info) {
-      return args.a + args.b
+      if (args.numbers.length === 0) {
+        return 0
+      }
+
+      return args.numbers.reduce((accumulator, currentValue) => {
+        return accumulator + currentValue
+      })
+    },
+    grades(parent, args, ctx, info) {
+      return [99, 80, 93]
     },
     me() {
       return {
