a7ad82e984d051b026355a10ece3e0308b478703
Jesse Rykerson <rykerson@gmail.com>
2021-01-23 21:50:58 -0500

    Working with Arrays: Part II

--- src/index.js
+++ src/index.js
@@ -2,12 +2,63 @@ import { GraphQLServer } from 'graphql-yoga'
 
 // 5 Scalar types (single value): String, Boolean, Int, Float, ID,
 
+// 1. Set up an array of three posts with dummy post data (id, title, body, published)
+//
+// 2. Set up a "posts" query and resolver that returns all the posts
+//
+// 3. Test the query out
+//
+// 4. Add a "query" argument that only returns posts that contain the query string in the title or body
+//
+// 5. Run a few sample queries searching for posts with a specific title
+
+// Demo user data
+const users = [
+  {
+    id: '1',
+    name: 'Jesse',
+    email: 'jesse@example.com',
+    age: 38,
+  },
+  {
+    id: '2',
+    name: 'Dylan',
+    email: 'dylan@example.com',
+  },
+  {
+    id: '3',
+    name: 'Mike',
+    email: 'mike@example.com',
+  },
+]
+
+// Demo post data
+const posts = [
+  {
+    id: '1',
+    title: 'My first post',
+    body: 'This is my first example posts to use for testing!',
+    published: true,
+  },
+  {
+    id: '2',
+    title: 'My second post',
+    body: 'Another example of posting.',
+    published: false,
+  },
+  {
+    id: '3',
+    title: 'My last post',
+    body: 'The last time a post will be tested (for now).',
+    published: true,
+  },
+]
+
 // Type definitions (Schema)
 const typeDefs = `
   type Query {
-    greeting(name: String, position: String): String!
-    add(numbers: [Float!]!): Float!
-    grades: [Int!]!
+    users(query: String): [User!]!
+    posts(query: String): [Post!]!
     me: User!
     post: Post!
   }
@@ -30,25 +81,26 @@ const typeDefs = `
 // Resolvers
 const resolvers = {
   Query: {
-    greeting(parent, args, ctx, info) {
-      console.log('args: ', args)
-      if (args.name && args.position) {
-        return `Hello, ${args.name}! You are my favorite ${args.position}.`
-      } else {
-        return 'Hello!'
-      }
-    },
-    add(parent, args, ctx, info) {
-      if (args.numbers.length === 0) {
-        return 0
+    users(parent, args, ctx, info) {
+      if (!args.query) {
+        return users
       }
 
-      return args.numbers.reduce((accumulator, currentValue) => {
-        return accumulator + currentValue
+      return users.filter(user => {
+        return user.name.toLowerCase().includes(args.query.toLowerCase())
       })
     },
-    grades(parent, args, ctx, info) {
-      return [99, 80, 93]
+    posts(parent, args, ctx, info) {
+      if (!args.query) {
+        return posts
+      }
+
+      return posts.filter(post => {
+        return (
+          post.title.toLowerCase().includes(args.query.toLowerCase()) ||
+          post.body.toLowerCase().includes(args.query.toLowerCase())
+        )
+      })
     },
     me() {
       return {
