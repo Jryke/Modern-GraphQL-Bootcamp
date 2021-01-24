85d1d7292f5b4959637d2f8d5a8d3600c84867c7
Jesse Rykerson <rykerson@gmail.com>
2021-01-24 11:47:23 -0500

    Relational Data: Arrays

--- src/index.js
+++ src/index.js
@@ -71,6 +71,7 @@ const typeDefs = `
     name: String!
     email: String!
     age: Int
+    posts: [Post!]!
   }
 
   type Post {
@@ -127,6 +128,13 @@ const resolvers = {
     author(parent, args, ctx, info) {
       return users.find(user => {
         return user.id === parent.author
+      })
+    },
+  },
+  User: {
+    posts(parent, args, ctx, info) {
+      return posts.filter(post => {
+        return post.author === parent.id
       })
     },
   },
