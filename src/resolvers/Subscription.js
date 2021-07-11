c025a5b2d54be595fd7b70ce67cff340c8b50c55
Jesse Rykerson <rykerson@gmail.com>
2021-07-11 11:16:44 -0400

    Expanding the Posts Subscription for Edits and Deletions

--- src/resolvers/Subscription.js
+++ src/resolvers/Subscription.js
@@ -1,13 +1,3 @@
-//
-// Goal: Create a subscription for new post
-//
-// 1. Define "post" subscription. No arguments are necessary. Response should be a post object.
-// 2. Set up the resolver for post. Since there are no args, a channel name like "post" is fine.
-// 3. Modify the mutation for creating a post to publish the new post data.
-//  - Only call pubsub.publish if the post had "published" set to true.
-//  - Don't worry about updatePost or deletePost.
-// 4. Test your work!
-
 const Subscription = {
   count: {
     subscribe(parent, args, { pubsub }, info) {
